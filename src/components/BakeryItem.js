c0694a38dab46204e332399ce87e0f269da862ff
sncriad <77998632+sncriad@users.noreply.github.com>
2022-11-09 20:33:13 -0500

    frik

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,14 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem(item, index, img) {
+    console.log(index);
+    console.log(item);
+    console.log(item["image"]);
+	return (
+		<div>
+            <img src={require(img)} className="App-logo" alt="logo" />
+            <h2>{item["name"]}</h2>
+            <p>{item["description"]}</p>
+            <p>{item["price"]}</p>
+        </div>
+	);
+}
