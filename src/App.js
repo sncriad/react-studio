41250f599f01799bfad37bc9025e8c43e45d7a43
Sebastian Criado <sebastiancriado@sebastians-air.devices.brown.edu>
2022-11-10 22:11:29 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,40 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-
+  const [tp, setIndex] = useState(0);
+  const [tracker, incAt] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  function handleNextClick(val) {
+    setIndex(tp + val);
+  }
+  function handleInc(ind) {
+    var acopy = tracker;
+    console.log(acopy);
+    acopy[ind] = acopy[ind] + 1;
+    console.log(acopy);
+    incAt(acopy);
+  }
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+      <div class="div-1">
+        {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+          <div>
+            <button onClick={() => {handleNextClick(item.price); handleInc(index)}}>
+              {BakeryItem(item, index, item.image)}
+            </button>
+          </div>
+        ))}
+      </div>
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <p> PRICE:{tp} </p>
+        {bakeryData.map((item, index) => (
+          <div>  
+          {item.name + " " + tracker[index]}
+          <br></br>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,11 +1,8 @@
 // TODO: create a component that displays a single bakery item
 export default function BakeryItem(item, index, img) {
-    console.log(index);
-    console.log(item);
-    console.log(item["image"]);
 	return (
 		<div>
-            <img src={require(img)} className="App-logo" alt="logo" />
+            <img src={item.image} style={{width:"100%"}}/>
             <h2>{item["name"]}</h2>
             <p>{item["description"]}</p>
             <p>{item["price"]}</p>
